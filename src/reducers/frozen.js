de0860c8b327a1a9fe5e3f13ffe7884aa512db81
Carlos Mertens <mertenscarlos@yahoo.com>
2020-06-10 23:44:30 +0200

    Add some data to frozen reducer

--- src/reducers/frozen.js
+++ src/reducers/frozen.js
@@ -2,6 +2,8 @@
 // 1. Current State, usually provide a default state
 // 2. Info that came from any action
 
-export default (state = [], action) => {
+const seedData = ["Lazy dinners", "Frozen Veggies", "Frozen Pizza"];
+
+export default (state = seedData, action) => {
   return state;
 };
