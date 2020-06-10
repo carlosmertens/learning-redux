120d897d3d236ff892b1970fea0756fc100d6a27
Carlos Mertens <mertenscarlos@yahoo.com>
2020-06-10 23:17:00 +0200

    Create FrozenDept component

---
+++ src/components/FrozenDept.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class FrozenDept extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>FrozenDept component</h1>
+      </div>
+    );
+  }
+}
+
+export default FrozenDept;
