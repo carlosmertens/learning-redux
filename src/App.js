f8b58de48675a1d6a78b19eda271a334e2b43e0e
Carlos Mertens <mertenscarlos@yahoo.com>
2020-06-13 00:10:33 +0200

    Add Main component to count item in the entire store

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import "./App.css";
 import FrozenDept from "./components/FrozenDept";
 import ProduceDept from "./components/ProduceDept";
 import MeatDept from "./components/MeatDept";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
-      <FrozenDept />
-      <ProduceDept />
-      <MeatDept />
-    </div>
+    <Router>
+      <div className='App'>
+        <Route path='/' component={NavBar} />
+        <Route path='/main' component={Main} />
+        <Route path='/frozen-dept' component={FrozenDept} />
+        <Route path='/produce-dept' component={ProduceDept} />
+        <Route path='/meat-dept' component={MeatDept} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Main extends Component {
+  render() {
+    // console.log(this.props.frozenData);
+    const frozenQuantity = this.props.frozenData.reduce(
+      (accum, frozenItem) => accum + frozenItem.quantity,
+      0
+    );
+    const produceQuantity = this.props.produceData.reduce(
+      (accum, produceItem) => accum + produceItem.quantity,
+      0
+    );
+    const meatQuantity = this.props.meatData.reduce(
+      (accum, meatItem) => accum + meatItem.quantity,
+      0
+    );
+    return (
+      <div>
+        <h2>FrozenDept: {frozenQuantity}</h2>
+        <h2>ProduceDept: {produceQuantity}</h2>
+        <h2>MeatDept: {meatQuantity}</h2>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    frozenData: state.frozen,
+    produceData: state.produce,
+    meatData: state.meat,
+  };
+}
+
+export default connect(mapStateToProps)(Main);
